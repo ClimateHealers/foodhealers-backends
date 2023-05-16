c47b01f9fd5df4f09599cbb0c39630dd4c4d87bb
Raiyan Firaz <rfiraz@alamanceinc.com>
2023-05-16 16:50:27 +0530

    Added Allowed host

--- foodhealers_backend/settings.py
+++ foodhealers_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-m+rp2up2513j@sq+^qdu&*y%9v1-sk*x2mwm-pq%_7s)c=pzv%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'climatehealers.alamanceinc.com', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://climatehealers.alamanceinc.com','https://*.127.0.0.1']
 
