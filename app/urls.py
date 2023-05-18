e86f81307759f2f38dcc882a951f2b7583efd869
raiyan firaz <rfiraz@alamanceinc.com>
2023-05-18 12:01:35 +0000

    Testcase/api test cases

--- app/urls.py
+++ app/urls.py
@@ -16,16 +16,16 @@ urlpatterns = [
     re_path(r'login/', views.SignIn.as_view(), name='user-login'),
     # POST (to view food Events occuring based on Start and End Date)
     re_path(r'find-food/', views.FindFood.as_view(), name='find-food'),
-    # GET (to View Events added by user )and POST (to Add Events)
+    # GET (to View Events added by user) and POST (to Add Events)
     re_path(r'event/', views.Event.as_view(), name='food-event'), 
-    # GET (to View Bookmarked Events added by user ) and POST (to Add Bookmarked Events)
-    re_path(r'bookmark/', views.BookmarkEvent.as_view(),),  
+    # GET (to View Bookmarked Events added by user) and POST (to Add Bookmarked Events)
+    re_path(r'bookmark/', views.BookmarkEvent.as_view(), name='bookmark-event'),  
     # GET (to fetch categories fo recipies)
-    re_path(r'categories/', views.Categories.as_view(),),
+    re_path(r'categories/', views.Categories.as_view(), name='fetch-category'),
     # GET (to View recipes) and POST (to Add recipes)
-    re_path(r'recipe/(?P<categoryId>[-\w]*)/', views.FindFoodRecipe.as_view(),),
+    re_path(r'recipe/(?P<categoryId>[-\w]*)/', views.FindFoodRecipe.as_view(), name='food-recipe'),
     # GET (to fetch requestTypes for Request API)
-    re_path(r'request-types/', views.RequestTypes.as_view(),),
+    re_path(r'request-types/', views.RequestTypes.as_view(), name='fetch-requestType'),
     # GET (to View food/Supplies Request) and POST (to Add food/Supplies Request)
     re_path(r'request-food/(?P<requestTypeId>[-\w]*)/', views.RequestFoodSupplies.as_view(),),  
     # GET (to View volunteers Request) and POST (to Add volunteers Request)
