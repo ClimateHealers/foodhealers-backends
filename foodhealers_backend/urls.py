9446abd35807488c3675486f7111d7cef2b3ca88
raiyan123 <rfiraz@alamanceinc.com>
2023-07-18 11:20:51 +0530

    repushing mogrations branch

--- foodhealers_backend/urls.py
+++ foodhealers_backend/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    url='https://api.climatehealers.com'
+    url='https://foohealers.climatehealers.com'
 )
 
 if settings.DEBUG :
