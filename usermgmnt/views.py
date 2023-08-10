5599d2d1540c418a5ae02be200ff360ea5746ea1
raiyan123 <raiyan.firaz@alamanceinc.com>
2023-08-10 15:48:26 +0530

    delete_user_account_action Function refactored

--- usermgmnt/views.py
+++ usermgmnt/views.py
@@ -23,58 +23,44 @@ class DeleteUserAccountView(APIView):
 
         return render(request, 'EmailConfirmationForUserDeletion.html', {'uniqueID': unique_id})
 
-def deleteUserAccountAction(request, uniqueID):
+def delete_user_account_action(request, uniqueID):
     template_name = 'ThankYouPage.html'
     try:
         firebase_user = auth.get_user(uniqueID)
-            
         email = firebase_user.email
-
-        if Volunteer.objects.filter(email=email).exists():
+        
+        try:
             user = Volunteer.objects.get(email=email)
-
+            
             # Delete Custom Token Object
             CustomToken.objects.filter(user=user).delete()
-
-            # Volunteer Profile Photo Deleted
-            if Document.objects.filter(volunteer=user, docType=DOCUMENT_TYPE[0][0]).exists():
-                allDocuments = Document.objects.filter(volunteer=user, docType=DOCUMENT_TYPE[0][0])
-                for doc in allDocuments:
-                    doc.delete()
             
-            # Volunteer Vehicle and Vehicle Photo Deleted
-            if Vehicle.objects.filter(owner=user).exists():
-                allVehicles = Vehicle.objects.filter(owner=user)
-                for vehicle in allVehicles:
-                    if Document.objects.filter(vehicle=vehicle, docType=DOCUMENT_TYPE[3][0]).exists():
-                        allVehicleDocs = Document.objects.filter(vehicle=vehicle, docType=DOCUMENT_TYPE[3][0])
-                        for vehicleDocs in allVehicleDocs:
-                            vehicleDocs.delete()
-                    vehicle.delete()
-
-            # Donations Deleted (Donation to be Deleted Before Food Events Deletion)
-            if Donation.objects.filter(donatedBy=user).exists():
-                allDonations = Donation.objects.filter(donatedBy=user)
-                for donation in allDonations:
-                    donation.delete()
-
-            # Food Event and Event Photo Deleted
-            if FoodEvent.objects.filter(createdBy=user).exists():
-                foodEvents = FoodEvent.objects.filter(createdBy=user)
-                for fEvent in foodEvents:
-                    if Document.objects.filter(docType=DOCUMENT_TYPE[1][0], event=fEvent).exists():
-                        allFoodEventDocs = Document.objects.filter(docType=DOCUMENT_TYPE[1][0], event=fEvent)
-                        for foodEventDocs in allFoodEventDocs:
-                            foodEventDocs.delete()
-                    fEvent.delete()
-
+            # Delete Volunteer-related documents
+            Document.objects.filter(volunteer=user, docType=DOCUMENT_TYPE[0][0]).delete()
+            
+            # Delete Volunteer Vehicles and related documents
+            vehicles = Vehicle.objects.filter(owner=user)
+            for vehicle in vehicles:
+                Document.objects.filter(vehicle=vehicle, docType=DOCUMENT_TYPE[3][0]).delete()
+            vehicles.delete()
+            
+            # Delete Donations
+            Donation.objects.filter(donatedBy=user).delete()
+            
+            # Delete Food Events and related documents
+            food_events = FoodEvent.objects.filter(createdBy=user)
+            for fEvent in food_events:
+                Document.objects.filter(docType=DOCUMENT_TYPE[1][0], event=fEvent).delete()
+            food_events.delete()
+            
             user.delete()
             auth.delete_user(uniqueID)
             
             return render(request, template_name, {'success': True, 'message': 'User has been successfully Deleted'})
-        else:
+        
+        except Volunteer.DoesNotExist:
             return render(request, template_name, {'success': False, 'message': 'User with Id does not exist'})
-
+        
     except Exception as e:
         return render(request, template_name, {'success': False, 'error': str(e)})
 
