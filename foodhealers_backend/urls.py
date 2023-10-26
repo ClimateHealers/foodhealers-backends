34b6e15ff5be9c5fff9cc305b9b806ca31e30a91
raiyan123 <134260615+raiyanfiraz@users.noreply.github.com>
2023-10-26 19:20:40 +0530

    Update urls.py

--- foodhealers_backend/urls.py
+++ foodhealers_backend/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    url='https://api.climatehealers.com'
+    url='https://foodhealers.climatehealers.com'
 )
 
 if settings.DEBUG :
